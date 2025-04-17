d8187aa4b85208e4896d4fd830e8ecec55801a97
0097eo <emmanuelokello294@gmail.com>
2025-04-17 21:48:34 +0300

    fix: Update API proxy target to use production URL

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://eshop-zkkr.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/'),
       }
